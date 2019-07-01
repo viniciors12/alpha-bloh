ff694de1c3efa8c46b651c2a6d729da0bd45e7f3
viniciors12 <viniciorodriguez97@gmail.com>
2019-07-01 15:27:58 -0600

    Add edit, update and show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+
+  resources :articles
 end
