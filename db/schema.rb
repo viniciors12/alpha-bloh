1e1cc7fcd84a351f5cfb259a40c984983ef5b261
viniciors12 <viniciorodriguez97@gmail.com>
2019-07-09 23:00:32 -0600

    Secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_211220) do
+ActiveRecord::Schema.define(version: 2019_07_10_045441) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_211220) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
